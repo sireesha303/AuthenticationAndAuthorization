51f7e20b75b9d92b6dd2f6e9f721e21bcfa83e76
sireesha <msireesha30031999@gmail.com>
2022-02-18 21:27:10 +0530

    modified few styles in cart and loginform components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
 
 
@@ -9,6 +11,8 @@ const App = () => (
     <Routes>
         <Route path="/login" element={<LoginForm/>} />
         <Route path="/" element={<Home/>}/>
+        <Route path="/products" element={<Products />}/>
+        <Route path="/cart" element={<Cart />}/>
     </Routes>
     </BrowserRouter>
 )
