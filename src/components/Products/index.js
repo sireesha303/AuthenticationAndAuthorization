0a038db5c274057fbf124e9126d17b1f466e3d2b
sireesha <msireesha30031999@gmail.com>
2022-02-18 21:26:03 +0530

    products componebt

---
+++ src/components/Products/index.js
@@ -0,0 +1,14 @@
+import Header from '../Header/index'
+import './index.css'
+
+const Products = () => (
+    <>
+    <Header />
+    <div className='product-container'>
+        <img src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png" alt="product-img" className='product-img'/>
+    </div>
+
+    </>
+)
+
+export default Products
