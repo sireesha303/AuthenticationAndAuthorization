b3b8bcd2b24b298c81479ea8397b8d817facc281
sireesha <msireesha30031999@gmail.com>
2022-02-18 21:14:46 +0530

    cart component

---
+++ src/components/Cart/index.js
@@ -0,0 +1,14 @@
+import Header from '../Header/index'
+import './index.css'
+
+const Cart = () => (
+    <>
+    <Header />
+    <div className='cart-container'>
+        <img src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png" alt="cart-img" className='cart-img'/>
+    </div>
+
+    </>
+)
+
+export default Cart
